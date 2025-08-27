5af9d795b8b5a5f534bb5a97593ce81ee3018b3d
Maksim Sasnouski <qeeqez@gmail.com>
2025-08-27 16:11:12 +0400

    RIXL Docs as a name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'RIXL Doc Island',
+  title: 'RIXL Docs',
   tagline: 'Your Next Media Cloud',
   favicon: 'img/favicon.ico',
 
