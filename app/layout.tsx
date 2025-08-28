0b9be0a930886d26e895f956b88c17f6b300f769
Maksim Sasnouski <qeeqez@gmail.com>
2025-08-28 04:51:25 +0400

    Allow static build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import '@/app/global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import {Provider} from "@/provider";
+import {Inter} from 'next/font/google';
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({children}: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-      </body>
+    <body className="flex flex-col min-h-screen">
+    <Provider>{children}</Provider>
+    </body>
     </html>
   );
 }
