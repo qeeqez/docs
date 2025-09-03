f674540e08ba2789bc9dbc04ed5e9c71c1693781
Maksim Sasnouski <qeeqez@gmail.com>
2025-09-03 18:21:33 +0400

    Bump @rixl/videosdk-react to 0.12.2

---
+++ components/rixl/video-mdx.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import {Video} from "@rixl/videosdk-react";
+
+interface Props {
+  id: string;
+}
+
+export const VideoMDX = ({id}: Props) => {
+  return <div className="flex overflow-hidden w-full h-full rounded-0.25 drop-shadow-xl max-w-fd-container">
+    <Video
+      id={id}
+      // className="w-full h-fit"
+      muted={false}
+      progressBar={true}
+      loop={false}
+      autoPlay={false}
+      allowPlayPause={true}
+      allowFullscreen={true}
+      allowPictureInPicture={true}
+      theme="default"
+      volume={1.0}
+    />
+  </div>;
+};
