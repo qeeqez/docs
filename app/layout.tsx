e6b7441bbc275bc2397bdd8118be4d26ac20a241
Maksim Sasnouski <qeeqez@gmail.com>
2025-09-10 16:56:20 +0400

    Prevent page extra scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from "next/font/google";
 import {HomeLayout} from "@/components/layout/home";
 import {baseOptions} from "@/lib/layout.shared";
 import {Provider} from "@/provider";
+import {cn} from "@/lib/cn";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -10,11 +11,17 @@ const inter = Inter({
 
 export default function Layout({children}: LayoutProps<"/">) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="text-foreground group/body overscroll-none font-sans antialiased [--footer-height:calc(var(--spacing)*14)] [--header-height:calc(var(--spacing)*14)] xl:[--footer-height:calc(var(--spacing)*24)]">
-        <Provider>
-          <HomeLayout {...baseOptions()}>{children}</HomeLayout>
-        </Provider>
+    <html lang="en" className={cn()} suppressHydrationWarning>
+      <body className={cn(
+        "group/body",
+        "overflow-hidden h-screen",
+        "text-foreground font-sans antialiased",
+      )}>
+        <div className="fixed left-0 right-0 top-0 bottom-0 h-screen w-screen overflow-auto overscroll-none"  >
+          <Provider>
+            <HomeLayout {...baseOptions()}>{children}</HomeLayout>
+          </Provider>
+        </div>
       </body>
     </html>
   );
