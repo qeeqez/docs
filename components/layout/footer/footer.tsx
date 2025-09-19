2de5b5adf84b44e3ec10ce576496c6ff20ee7678
Maksim Sasnouski <qeeqez@gmail.com>
2025-09-19 22:08:14 +0400

    Footer is now the part of page

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -1,10 +1,9 @@
-import {cn} from "@/lib/cn";
 import {FooterBottom} from "./footer-bottom";
 import {FooterContent} from "./footer-content";
 
 export function Footer() {
   return (
-    <footer className={cn("w-full mx-auto py-9 space-y-9 h-footer", "border-t bg-fd-background text-fd-foreground")}>
+    <footer className="w-full mx-auto py-9 space-y-9 border-t">
       <FooterContent/>
       <FooterBottom/>
     </footer>
--- components/layout/home/index.tsx
+++ components/layout/home/index.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import {NavProvider} from "fumadocs-ui/contexts/layout";
-import {type HTMLAttributes, useCallback, useEffect, useRef} from "react";
-import {Footer} from "@/components/layout/footer/footer";
+import type {HTMLAttributes} from "react";
 import {Header} from "@/components/layout/header/header";
+import {Background} from "@/components/layout/home/background";
 import {cn} from "@/lib/cn";
 import type {BaseLayoutProps, NavOptions} from "../shared/index";
-import {Background} from "@/components/layout/home/background";
 
 export interface HomeLayoutProps extends BaseLayoutProps, HTMLAttributes<HTMLElement> {
   nav?: Partial<
@@ -17,19 +16,17 @@ export interface HomeLayoutProps extends BaseLayoutProps, HTMLAttributes<HTMLEle
     enableHoverToOpen?: boolean;
   }
   >;
-  footer?: boolean;
 }
 
-export function HomeLayout({footer = true, nav = {}, links, githubUrl, i18n, searchToggle, ...rest}: HomeLayoutProps) {
+export function HomeLayout({nav = {}, links, githubUrl, i18n, searchToggle, ...rest}: HomeLayoutProps) {
   return (
     <NavProvider transparentMode={nav?.transparentMode}>
       <div id="nd-home-layout" {...rest} className={cn("relative z-10 flex min-h-svh flex-col", rest.className)}>
         <Background />
         <Header links={links} nav={nav} searchToggle={searchToggle} i18n={i18n} githubUrl={githubUrl}/>
-        <div className="w-full max-w-[92rem] mx-auto lg:px-8">
+        <div className="relative w-full max-w-[92rem] mx-auto lg:px-8">
           {rest.children}
         </div>
-        {footer && <Footer/>}
       </div>
     </NavProvider>
   );
