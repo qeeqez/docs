9922fa6db0af1c494215f50fe7b992dd2b2b3d8b
Maksim Sasnouski <qeeqez@gmail.com>
2025-09-21 03:53:18 +0400

    Improve style and coloring scheme

---
+++ components/layout/docs/sidebar/sidebar-separator.tsx
@@ -0,0 +1,16 @@
+import type {ComponentProps} from "react";
+import {cn} from "@/lib/cn";
+
+export function SidebarSeparator(props: ComponentProps<'p'>) {
+  return (
+    <h5
+      {...props}
+      className={cn(
+        'flex items-center gap-2.5 mb-2.5 p-2 ps-(--sidebar-item-offset) text-gray-900 dark:text-gray-200 text-sm font-medium empty:mb-0 [&_svg]:size-4 [&_svg]:shrink-0',
+        props.className,
+      )}
+    >
+      {props.children}
+    </h5>
+  );
+}
