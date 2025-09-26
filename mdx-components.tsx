1c3f3c212ecdb9bf76ef377d2171cefda626e4d1
Maksim Sasnouski <qeeqez@gmail.com>
2025-09-26 06:00:47 +0400

    Use our own components by default instead

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,27 +1,31 @@
 import Link from "fumadocs-core/link";
 import {Banner} from "fumadocs-ui/components/banner";
 import {Callout as FumadocsCallout} from "fumadocs-ui/components/callout";
-import {Card, Cards} from "fumadocs-ui/components/card";
 import {Step, Steps} from "fumadocs-ui/components/steps";
 import {Tab, Tabs} from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type {MDXComponents} from "mdx/types";
-import {Callout, Columns, InfoCard, InteractiveCard} from "@/components";
+import {Callout, Card, Columns} from "@/components";
 import {Video} from "@/components";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
-  const mdxComponents = { ...defaultMdxComponents };
+  const {
+    Card: _,
+    Cards: __,
+    Callout: ___,
+    ...mdxComponents
+  } = defaultMdxComponents;
+
+  const rixlComponents = {
+    Card,
+    Callout,
+    Columns,
+    Video,
+  };
 
   const customComponents = {
-    InteractiveCard,
-    InfoCard,
-    Callout,
-    Video,
-    Columns,
     Banner,
-    Card,
-    Cards,
     Tab,
     Tabs,
     Step,
@@ -33,6 +37,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...mdxComponents,
     ...customComponents,
+    ...rixlComponents,
     ...components,
   };
 }
