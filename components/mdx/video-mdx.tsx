25cdd31e9d3b18f4b4273b8311ead0623759d8dd
Maksim Sasnouski <qeeqez@gmail.com>
2025-09-26 04:36:59 +0400

    Dynamically load big components

---
+++ components/mdx/video-mdx.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import type {HTMLProps} from "react";
+
+const Video = dynamic(
+  () => import("@rixl/videosdk-react")
+    .then((mod) => mod.Video),
+  {ssr: true})
+
+declare type VideoTheme = "default" | "minimal" | "hideUI";
+
+declare interface VideoProps extends HTMLProps<HTMLVideoElement> {
+  id?: string;
+  progressBar?: boolean;
+  allowPlayPause?: boolean;
+  allowFullscreen?: boolean;
+  allowPictureInPicture?: boolean;
+  volume?: number;
+  theme?: VideoTheme;
+}
+
+export const VideoMDX = (props: VideoProps) => {
+  return <div className="flex overflow-hidden w-full h-[500px] rounded-0.25 drop-shadow-xl max-w-fd-container">
+    <Video {...props} />
+  </div>;
+};
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,13 +1,13 @@
+import Link from "fumadocs-core/link";
+import {Banner} from "fumadocs-ui/components/banner";
+import {Callout as FumadocsCallout} from "fumadocs-ui/components/callout";
+import {Card, Cards} from "fumadocs-ui/components/card";
+import {Step, Steps} from "fumadocs-ui/components/steps";
+import {Tab, Tabs} from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type {MDXComponents} from "mdx/types";
-import {VideoMDX} from "./components/rixl/video-mdx";
-import {InteractiveCard, InfoCard, Callout} from "./components/mdx-ui";
-import {Banner} from "fumadocs-ui/components/banner";
-import {Card, Cards} from "fumadocs-ui/components/card";
-import {Tab, Tabs} from "fumadocs-ui/components/tabs";
-import {Step, Steps} from "fumadocs-ui/components/steps";
-import {Callout as FumadocsCallout} from "fumadocs-ui/components/callout";
-import Link from "fumadocs-core/link";
+import {Callout, InfoCard, InteractiveCard } from "@/components";
+import {VideoMDX} from "@/components/mdx/video-mdx";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
