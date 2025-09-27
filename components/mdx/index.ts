06ea3d10a01d397d1bc7419a220cc20d38454580
Maksim Sasnouski <qeeqez@gmail.com>
2025-09-27 14:18:33 +0400

    Proper export

--- components/mdx/index.ts
+++ components/mdx/index.ts
@@ -1,5 +1,5 @@
+export {Callout} from "./callout"
 export {Card} from "./card";
 export {Columns} from "./columns";
 export {Icon} from "./icon";
 export {Video} from "./video";
-export { default as Callout } from "./callout"
