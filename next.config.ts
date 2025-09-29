1e123c2d4337c13e4c1a0a13d1cc96439caadd53
Yusuf Akinleye <ayaaakinleye@gmail.com>
2025-09-29 10:05:41 +0100

    #86euzz751 update config to stops turbopack from parsing js file map

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,33 @@
-import {createMDX} from "fumadocs-mdx/next";
-import type {NextConfig} from "next";
+import { createMDX } from "fumadocs-mdx/next";
+import type { NextConfig } from "next";
 
 const withMDX = createMDX();
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true
-  },
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
+    reactStrictMode: true,
+    output: "export",
+    images: {
+        unoptimized: true,
     },
-  },
+    productionBrowserSourceMaps: false,
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.map$/,
+            use: "ignore-loader",
+        });
+        return config;
+    },
+    turbopack: {
+        rules: {
+            "*.svg": {
+                loaders: ["@svgr/webpack"],
+                as: "*.js",
+            },
+            "*.map": {
+                loaders: ["ignore-loader"],
+            },
+        },
+    },
 };
 
 export default withMDX(nextConfig);
