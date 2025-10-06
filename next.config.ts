dea5768bd317c7548df164c340620477ebe0bf5d
Maksim Sasnouski <qeeqez@gmail.com>
2025-10-06 20:34:29 +0400

    #86euzz751 Remove redundant config

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,6 @@ const nextConfig: NextConfig = {
         unoptimized: true,
     },
     productionBrowserSourceMaps: false,
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.map$/,
-            use: "ignore-loader",
-        });
-        return config;
-    },
     turbopack: {
         rules: {
             "*.svg": {
@@ -24,7 +17,7 @@ const nextConfig: NextConfig = {
                 as: "*.js",
             },
             "*.map": {
-                loaders: ["ignore-loader"],
+                loaders: ["raw-loader"],
             },
         },
     },
