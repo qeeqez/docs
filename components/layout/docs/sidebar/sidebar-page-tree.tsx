39de64faf412d55b01dc5ee8e754c780fd109138
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2025-10-28 13:43:28 +0100

    Reduce sdk_route_pattern occurrence

--- components/layout/docs/sidebar/sidebar-page-tree.tsx
+++ components/layout/docs/sidebar/sidebar-page-tree.tsx
@@ -10,65 +10,66 @@ import {PageTreeFolder} from "@/components/layout/docs/sidebar/sidebar-page-tree
 import {SidebarSeparator} from "@/components/layout/docs/sidebar/sidebar-separator";
 import {cn} from "@/lib/cn";
 
-export function SidebarPageTree(props: {components?: Partial<SidebarComponents>}) {
-  const {root} = useTreeContext();
-  const pathname = usePathname();
+export function SidebarPageTree(props: { components?: Partial<SidebarComponents> }) {
+    const {root} = useTreeContext();
+    const pathname = usePathname();
 
-  return useMemo(() => {
-    const {Separator, Item, Folder} = props.components ?? {};
+    return useMemo(() => {
+        const {Separator, Item, Folder} = props.components ?? {};
 
-    const isSdkRoute = /\/[a-zA-Z]{2}\/sdk(\/|$)/.test(pathname ?? "");
+        const SDK_ROUTE_PATTERN = /\/[a-zA-Z]{2}\/sdk(\/|$)/;
+        const isSdkRoute = SDK_ROUTE_PATTERN.test(pathname ?? "");
 
-    const filteredItems = isSdkRoute
-      ? root.children
-      : root.children.filter((node) => {
-          if (node.type === "folder") {
-            const hasSdk =
-              (node.index?.url && /\/[a-zA-Z]{2}\/sdk(\/|$)/.test(node.index.url)) ||
-              node.children.some((child) => child.type === "page" && /\/[a-zA-Z]{2}\/sdk(\/|$)/.test(child.url));
-            return !hasSdk;
-          }
-          if (node.type === "page") return !/\/[a-zA-Z]{2}\/sdk(\/|$)/.test(node.url);
-          return true;
-        });
+        const filteredItems = isSdkRoute
+            ? root.children
+            : root.children.filter((node) => {
+                if (node.type === "folder") {
+                    const hasSdk =
+                        (node.index?.url && SDK_ROUTE_PATTERN.test(node.index.url)) ||
+                        node.children.some((child) => child.type === "page" && SDK_ROUTE_PATTERN.test(child.url));
+                    return !hasSdk;
+                }
+                if (node.type === "page") return !SDK_ROUTE_PATTERN.test(node.url);
+                return true;
+            });
 
-    function renderSidebarList(items: PageTree.Node[], level: number): ReactNode[] {
-      return items.map((item, i) => {
-        if (item.type === "separator") {
-          if (Separator) return <Separator key={i} item={item} />;
-          return (
-            <SidebarSeparator key={i} className={cn(i !== 0 && "mt-6 lg:mt-8")}>
-              {item.icon}
-              {item.name}
-            </SidebarSeparator>
-          );
+        function renderSidebarList(items: PageTree.Node[], level: number): ReactNode[] {
+            return items.map((item, i) => {
+                if (item.type === "separator") {
+                    if (Separator) return <Separator key={i} item={item}/>;
+                    return (
+                        <SidebarSeparator key={i} className={cn(i !== 0 && "mt-6 lg:mt-8")}>
+                            {item.icon}
+                            {item.name}
+                        </SidebarSeparator>
+                    );
+                }
+
+                if (item.type === "folder") {
+                    const children = renderSidebarList(item.children, level + 1);
+
+                    if (Folder)
+                        return (
+                            <Folder key={i} item={item} level={level}>
+                                {children}
+                            </Folder>
+                        );
+                    return (
+                        <PageTreeFolder key={i} item={item}>
+                            {children}
+                        </PageTreeFolder>
+                    );
+                }
+
+                if (Item) return <Item key={item.url} item={item}/>;
+                return (
+                    <SidebarItem key={item.url} href={item.url} external={item.external} icon={item.icon}>
+                        {item.name}
+                    </SidebarItem>
+                );
+            });
         }
 
-        if (item.type === "folder") {
-          const children = renderSidebarList(item.children, level + 1);
-
-          if (Folder)
-            return (
-              <Folder key={i} item={item} level={level}>
-                {children}
-              </Folder>
-            );
-          return (
-            <PageTreeFolder key={i} item={item}>
-              {children}
-            </PageTreeFolder>
-          );
-        }
-
-        if (Item) return <Item key={item.url} item={item} />;
-        return (
-          <SidebarItem key={item.url} href={item.url} external={item.external} icon={item.icon} >
-            {item.name}
-          </SidebarItem>
-        );
-      });
-    }
-
-    return <Fragment key={root.$id}>{renderSidebarList(filteredItems, 1)}</Fragment>;
-  }, [props.components, root, pathname]);
+        return <Fragment key={root.$id}>{renderSidebarList(filteredItems, 1)}</Fragment>;
+    }, [props.components, root, pathname]);
 }
