8220d47024f74b12c69363e67bc80d32b7d55ef1
Maksim Sasnouski <qeeqez@gmail.com>
2025-12-09 03:30:34 +0400

    fix: proper component imports
    #86ev2rk2x

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -2,3 +2,5 @@ export {Callout} from "./callout";
 export {Card} from "./card";
 export {Columns} from "./columns";
 export {Video} from "./video";
+export {Image} from "@rixl/videosdk-react";
+export {Feed} from "@rixl/videosdk-react";
