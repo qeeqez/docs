02ea462b9542f56515e8f56501e7c03df9bf15cf
Maksim Sasnouski <qeeqez@gmail.com>
2026-01-16 17:26:10 +0400

    chore: fix linting issues

--- src/components/layout/docs/sidebar/sidebar-page-tree.tsx
+++ src/components/layout/docs/sidebar/sidebar-page-tree.tsx
@@ -4,14 +4,14 @@ import type {Node} from "fumadocs-core/page-tree";
 import {useTreeContext} from "fumadocs-ui/contexts/tree";
 import {Fragment, type ReactNode, useMemo} from "react";
 import {SidebarItem} from "@/components/layout/docs/sidebar/sidebar-item";
-import {PageTreeFolder} from "@/components/layout/docs/sidebar/sidebar-page-tree-folder";
+// import {PageTreeFolder} from "@/components/layout/docs/sidebar/sidebar-page-tree-folder"; // TODO FIX SIDEBAR
 import {SidebarSeparator} from "@/components/layout/docs/sidebar/sidebar-separator";
 import {cn} from "@/lib/cn";
 
 export function SidebarPageTree() {
   const {root} = useTreeContext();
   return useMemo(() => {
-    function renderSidebarList(items: Node[], level: number): ReactNode[] {
+    function renderSidebarList(items: Node[], _level: number): ReactNode[] {
       return items.map((item, i) => {
         if (item.type === "separator") {
           return (
@@ -23,9 +23,9 @@ export function SidebarPageTree() {
         }
 
         if (item.type === "folder") {
-          const children = renderSidebarList(item.children, level + 1);
+          // const children = renderSidebarList(item.children, level + 1);
           return (
-            <div></div> // TODO FIX SIDEBAR
+            <div key={i}></div> // TODO FIX SIDEBAR
           ); // <PageTreeFolder key={i} item={item}>
           //   {children}
           // </PageTreeFolder>
